34e3d9eb3c671c10d45e53a280747c06e204b04e
HMH <musicbyhmh@gmail.com>
2023-02-06 14:44:54 +0100

    maj

--- Config/Controller.php
+++ Config/Controller.php
@@ -5,6 +5,8 @@ class Controller
     public $request;
     public $vars = array();
 
+    public $layout = 'base';
+
     public function __construct($request)
     {
         $this->request = $request;
@@ -17,13 +19,17 @@ class Controller
 
         $view = __ROOT__ . __DS__ . 'Template' . __DS__ . $this->request->controller . __DS__ . $view . '.php';
 
+        ob_start();
+
         require $view;
+
+        $content_for_layout = ob_get_clean();
+
+        require __ROOT__ . __DS__ . 'Template' . __DS__ . $this->layout . '.php';
     }
 
     public function set($key, $value = null)
     {
-        
-
         if(is_array($key))
         {
             $this->vars += $key;
