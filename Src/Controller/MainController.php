4c3f1b072d820890ef9f46f5b86d4ee4502d75f4
HMH <musicbyhmh@gmail.com>
2023-02-06 14:26:55 +0100

    maj MainController

--- Src/Controller/MainController.php
+++ Src/Controller/MainController.php
@@ -5,7 +5,7 @@ class MainController extends Controller
     public function view($nom)
     {
         $this->set(array(
-            'phrase' => 'Yo yo yo, c\'est moi le MainController',
+            'phrase' => 'Yo yo yo, \'ssup',
             'nom' => 'John Doe'
         ));
 
