b3e3947b99be02fd1d8fd22b9458ff1509942413
HMH <musicbyhmh@gmail.com>
2023-02-07 11:04:45 +0100

    maj

--- Config/Model.php
+++ Config/Model.php
@@ -2,13 +2,23 @@
 
 class Model
 {
+    public $db = 'default';
+
+
     public function __construct()
     {
-        //print_r(ConnexionDB::$database);
+        $conf = ConnexionDB::$database[$this->db];
+        try
+        {
+        $pdo = new PDO('mysql:host='.$conf['host'].';dbname='.$conf['database'], $conf['user'], $conf['password'], array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+        }catch(PDOException $e)
+        {
+            die($e->getMessage());
+        }
     }
     public function find($req)
     {
-        $sql = 'SELECT * FROM posts ';
+        $sql = 'SELECT * FROM posts';
 
         if(isset($req['conditions']))
         {
