e638042a1360e694f487f75e675436214be4bc9a
HMH <musicbyhmh@gmail.com>
2023-02-08 12:43:11 +0100

    màj

--- Src/Controller/PostsController.php
+++ Src/Controller/PostsController.php
@@ -4,13 +4,15 @@ class PostsController extends Controller
 {
     function index()
     {
+        $perPage = 1;
         $this->loadModel('Post');
+        $conditions = ['online' => 1, 'type' => 'post'];
         $d['posts'] = $this->Post->find(array(
-            'conditions' => ['online' => 1, 
-                            'type' => 'post']
+            'conditions' => $conditions,
+            'limit' => ($perPage * ($this->request->page - 1)) . ',' . $perPage,
         ));
         $d['total'] = $this->Post->findCount($conditions);
-        $d['page'] = '20';
+        $d['page'] = ceil($d['total'] / $perPage);
         $this->set($d);
     }
 
