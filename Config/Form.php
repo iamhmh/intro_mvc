f02c296318de56fe29b3691e482bfa5184083e7e
HMH <musicbyhmh@gmail.com>
2023-02-13 12:57:56 +0100

    repo

--- Config/Form.php
+++ Config/Form.php
@@ -1,41 +1,50 @@
 <?php
 
-class Form
+class Form 
 {
     public $controller;
+
     public function __construct($controller)
     {
         $this->controller = $controller;
+        
     }
 
     public function input($name, $label, $options = [])
     {
         $html = '<div class="clearfix">
-                    <label for="input'.$name.'">'.$label.'</label>
-                    <div class="input">';
+                <label for="input'.$name.'">'.$label.'</label>
+                    <div class="input"> '; 
+
         $attr = ' ';
+
+        // Pour parcourir le tableaux des options
         foreach($options as $k => $v)
         {
             if($k != 'type')
             {
-                $attr .= "$k=\"$v\"";
+              $attr .= "$k=\"$v\"";  
             }
         }
-        if(!isset($options['type']))
+
+        // input simple
+        if(!isset($options['type'])) 
         {
-            $html .=    '   <input type="text" name="'.$name.'" value="'.$this->controller->request->data->$name.'" id="input'.$name.'" '.$attr.'>';
-        }
+            $html .= ' <input type="text" name="'.$name.'" value="'.$this->controller->request->data->$name.'" id="input'.$name.'" '.$attr.'>';
+        }      
+        // text area
         elseif($options['type'] == 'textarea')
         {
-            $html .=    '   <textarea type="" name="'.$name.'" id="input'.$name.'" '.$attr.'>'.$this->controller->request->data->$name.'</textarea>';
-        }
+            $html .= ' <textarea type="" name="'.$name.'" id="input'.$name.'" '.$attr.'>'.$this->controller->request->data->$name.'</textarea>';
+        } 
         elseif($options['type'] == 'checkbox')
-        {
-            $html .=    '   <input type="hidden" name="'.$name.'" value="0">
-                            <input type="checkbox name="'.$name.'" value="1" '.(empty($value)) ? '' : 'checked'.'>';
-        }
-        $html .=    '</div>
-                    </div>';
+            {
+                $html .= '<input type="hidden" name="'.$name.'" value="0"> 
+                          <input type="checkbox" name="'.$name.'" value="1" '.(empty($value)? '' : 'checked').'>';
+            }    
+        $html .= '  </div>
+                    </div>';   
+                    
         return $html;
     }
 }
--- Template/posts/admin_edit.php
+++ Template/posts/admin_edit.php
@@ -3,6 +3,10 @@
 </div>
 <form method="post" action="<?php echo Router::url('admin/posts/edit/'); ?>">
     <?php echo $this->Form->input('name', 'Titre');?>
+    <?php echo $this->Form->input('slug', 'slug');?>
     <?php echo $this->Form->input('content', 'Contenu', ['type' => 'textarea', 'rows' => '7', 'cols' => '50']);?>
-    <?php echo $this->Form->input('online', 'En ligne', ['type' => 'checkbox']);?>
+    <?php echo $this->Form->input('online', 'en ligne', ['type' => 'checkbox']);?>
+    <div class="action">
+        <input type="submit" class="btn-primary" value="Envoyer">
+    </div>
 </form>
