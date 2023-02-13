acc205f28a8158e6668fcce3317e2a1dc52c21c4
HMH <musicbyhmh@gmail.com>
2023-02-13 12:33:29 +0100

    piaf

--- Config/Form.php
+++ Config/Form.php
@@ -7,18 +7,27 @@ class Form
     {
         $this->controller = $controller;
     }
+
     public function input($name, $label, $options = [])
     {
         $html = '<div class="clearfix">
                     <label for="input'.$name.'">'.$label.'</label>
                     <div class="input">';
+        $attr = ' ';
+        foreach($options as $k => $v)
+        {
+            if($k != 'type')
+            {
+                $attr .= "$k=\"$v\"";
+            }
+        }
         if(!isset($options['type']))
         {
-            $html .=    '   <input type="text" name="'.$name.'" value="'.$this->controller->request->data->$name.'" id="input'.$name.'">';
+            $html .=    '   <input type="text" name="'.$name.'" value="'.$this->controller->request->data->$name.'" id="input'.$name.'" '.$attr.'>';
         }
         elseif($options['type'] = 'textarea')
         {
-            $html .=    '   <textarea type="" name="'.$name.'" id="input'.$name.'">'.$this->controller->request->data->$name.'</textarea>';
+            $html .=    '   <textarea type="" name="'.$name.'" id="input'.$name.'" '.$attr.'>'.$this->controller->request->data->$name.'</textarea>';
         }
         $html .=    '</div>
                     </div>';
--- Template/posts/admin_edit.php
+++ Template/posts/admin_edit.php
@@ -3,5 +3,5 @@
 </div>
 <form method="post" action="<?php echo Router::url('admin/posts/edit/'); ?>">
     <?php echo $this->Form->input('name', 'titre');?>
-    <?php echo $this->Form->input('content', 'contenu', ['type' => 'textarea', 'row' => '5', 'col' => '10']);?>
+    <?php echo $this->Form->input('content', 'contenu', ['type' => 'textarea', 'rows' => '7', 'cols' => '50']);?>
 </form>
