be925b31aa3a997e2e2ce7ffedda4fd0ec2155b2
HMH <musicbyhmh@gmail.com>
2023-02-14 12:54:51 +0100

    maj

--- Template/layout/admin.php
+++ Template/layout/admin.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html> 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr"> 
-    <head> 
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/> 
-    <title><?php echo isset($title_for_layout)?$title_for_layout:'Administration'; ?></title> 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    </head> 
-    <body>       
+<head> 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/> 
+<title><?php echo isset($title_for_layout)?$title_for_layout:'Administration'; ?></title> 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head> 
+  <body>       
       
         <div class="topbar" style="position:static"> 
           <div class="topbar-inner"> 
@@ -22,7 +22,7 @@
  
         <div class="container" style="padding-top:60px;">
             <?php echo $this->Session->flash(); ?>
-        	<?php echo $content_for_layout; ?>
+        	  <?php echo $content_for_layout; ?>
         </div>
          
     <script src="<?php echo Router::webroot('js/tinymce/tinymce.min.js'); ?>"></script>
@@ -47,5 +47,5 @@
             content_css: 'css/content.css'
         });
     </script>
-    </body>
+  </body>
 </html>
